6e32e8da175cbec2d78f6b8bef6c4336c6ca5ad1
Timam <me@timam.io>
2018-08-06 04:21:24 +0600

    display color video

--- capture-video.py
+++ capture-video.py
@@ -5,11 +5,18 @@ cap = cv2.VideoCapture(0)
 
 while(True):
     ret, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    cv2.imshow('frame',gray)
+    hsv1 = cv2.cvtColor(frame, cv2.COLOR_RGB2HSV)
+    h, s, v = cv2.split(hsv1)
+    hsv2 = cv2.merge((h, s, v))
+    color = cv2.cvtColor(hsv2, cv2.COLOR_HSV2RGB)
+    cv2.imshow('RGB Video',color)
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('Gray Video', gray)
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 cap.release()
-cv2.destroyAllWindows()
+cv2.destroyAllWindolsws()
