dbf45752e7b0526b1998c22d87300c9993efc313
Timam <me@timam.io>
2018-08-06 04:36:55 +0600

    video capture improved

--- capture-video.py
+++ capture-video.py
@@ -7,12 +7,10 @@ while(True):
     ret, frame = cap.read()
 
     hsv1 = cv2.cvtColor(frame, cv2.COLOR_RGB2HSV)
-    h, s, v = cv2.split(hsv1)
-    hsv2 = cv2.merge((h, s, v))
-    color = cv2.cvtColor(hsv2, cv2.COLOR_HSV2RGB)
+    color = cv2.cvtColor(hsv1, cv2.COLOR_HSV2RGB)
     cv2.imshow('RGB Video',color)
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(color, cv2.COLOR_RGB2GRAY)
     cv2.imshow('Gray Video', gray)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
